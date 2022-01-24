7a96b8eac5052c2411b7b9259cee8a372bac2275
Rafhael <rafhael.dev@gmail.com>
2022-01-24 08:49:09 -0300

    reducing log chatter

--- src/main/java/com/studies/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/com/studies/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,7 +2,6 @@ package com.studies.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
 @SpringBootApplication
 public class MsscBeerServiceApplication {
