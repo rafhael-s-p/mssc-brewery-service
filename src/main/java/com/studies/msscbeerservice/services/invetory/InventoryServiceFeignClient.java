6d77e172be0019a515da7a2d6cf6f0029df59194
Rafhael <rafhael.dev@gmail.com>
2022-03-02 09:56:04 -0300

    configuring openfeign client

---
+++ src/main/java/com/studies/msscbeerservice/services/invetory/InventoryServiceFeignClient.java
@@ -0,0 +1,19 @@
+package com.studies.msscbeerservice.services.invetory;
+
+import com.studies.msscbeerservice.services.invetory.model.BeerInventoryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.UUID;
+
+@FeignClient(name = "inventory-service")
+public interface InventoryServiceFeignClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
+    ResponseEntity<List<BeerInventoryDto>> getOnhandInventory(@PathVariable UUID beerId);
+
+}
